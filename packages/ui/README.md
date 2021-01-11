9586f7c77c993d2a9630f9f2e919656a5c6fd08a
unknown <Bowen>
2021-01-11 14:44:58 +0800

    修复Bug

---
+++ packages/ui/README.md
@@ -0,0 +1,3 @@
+# ZZZ-UI
+
+> build for taro
--- packages/ui/build/rollup.config.js
+++ packages/ui/build/rollup.config.js
@@ -1,57 +1,63 @@
 import nodePath from "path";
 import json from "@rollup/plugin-json";
-import nodeResolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import ts from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy";
 import pkg from "./../package.json";
 
-const resolveFile = path => nodePath.resolve(__dirname, '..', path);
+const resolveFile = (path) => nodePath.resolve(__dirname, "..", path);
+
+console.log(resolveFile("src/styles"));
 
 export default {
   input: resolveFile(pkg.source),
-  output: [{
-    file: resolveFile(pkg.main),
-    format: "cjs",
-    sourcemap: true
-  }, {
-    file: resolveFile(pkg.module),
-    format: "es",
-    sourcemap: true
-  }, {
-    file: resolveFile(pkg.browser),
-    format: "umd",
-    sourcemap: true,
-    name: "taroColorUi"
-  }],
+  output: [
+    {
+      file: resolveFile(pkg.main),
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: resolveFile(pkg.module),
+      format: "es",
+      sourcemap: true,
+    },
+    {
+      file: resolveFile(pkg.browser),
+      format: "umd",
+      sourcemap: true,
+      name: "taroColorUi",
+    },
+  ],
   external: [
     "react",
     "react-dom",
     "@tarojs/components",
     "@tarojs/runtime",
     "@tarojs/taro",
-    "@tarojs/react"
+    "@tarojs/react",
   ],
   plugins: [
     nodeResolve({
       customResolveOptions: {
-        moduleDirectory: 'node_modules'
-      }
+        moduleDirectory: "node_modules",
+      },
     }),
     commonjs({
-      include: /\/node_modules\//
+      include: /\/node_modules\//,
     }),
     json(),
     ts({
-      tsconfig: resolveFile("tsconfig.rollup.json")
+      tsconfig: resolveFile("tsconfig.rollup.json"),
     }),
     copy({
       targets: [
         {
-          src: resolveFile("src/style"),
-          dest: resolveFile("dist")
-        }
-      ]
-    })
-  ]
+          src: "src/styles",
+          dest: resolveFile("dist"),
+        },
+      ],
+    }),
+  ],
 };
